500a25f6fde9e1c09e986a67922abcc1d591b3bc
Steven Auguto Villegas Castellanos <steven@Multivac-2.0>
2020-05-17 00:41:05 -0500

    add store folder

---
+++ src/store/store.js
@@ -0,0 +1,19 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from './domains';
+import rootSaga from '../sagas';
+
+export const configureStore = () => {
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = applyMiddleware(sagaMiddleware)
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  const store = createStore(
+    rootReducer,
+    composeEnhancers(middlewares)
+  );
+  // run rootSaga to handle side effects 
+  sagaMiddleware.run(rootSaga)
+
+  return store
+}
