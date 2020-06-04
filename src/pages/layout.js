5cbc9c58129de17e1211d0d562ed9b8998e090c6
Steven Augusto Villegas Castellanos <sagrath23@gmail.com>
2020-06-04 00:01:09 -0500

    improve distribution

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,5 +1,6 @@
 import React, { createContext, useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import {
@@ -13,7 +14,19 @@ import { CartSidebar } from '../components/CartSidebar';
 
 export const PageContext = createContext({ pageContext: {}, filterContext: {}, setFilterContext: () => {}});
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(10),
+  }
+}));
+
 export const Layout = () => {
+  const theme = useTheme();
+  const classes = useStyles(theme);
   const [isSidebarOpen, toggleSidebar] = useState(false);
   const [filterContext, setFilterContext] = useState({ inStock: false, productName: '' });
   // TODO: improve context structure (moving setFilterContext inside  a proper property)
@@ -27,12 +40,12 @@ export const Layout = () => {
   };
 
   return (
-    <>
+    <div className={classes.root}>
       <CssBaseline />
       <PageContext.Provider value={pageContextValue}>
         <CartHeader />
         <CartSidebar />
-        <Container maxWidth="xl">
+        <Container className={classes.content} maxWidth="xl">
           <Switch>
             <Route exact path="/cart" component={Cart} />
             <Route exact path="/products/:productId" component={ProductDetail} />
@@ -41,6 +54,6 @@ export const Layout = () => {
           </Switch>
         </Container>
       </PageContext.Provider>
-    </>
+    </div>
   );
 };
