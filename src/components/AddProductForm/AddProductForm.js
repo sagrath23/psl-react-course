def6fbaa890b025dfe9b6b4a4f4f2cacb15ff7d6
Steven Augusto Villegas Castellanos <sagrath23@gmail.com>
2020-06-09 00:50:28 -0500

    improve form

--- src/components/AddProductForm/AddProductForm.js
+++ src/components/AddProductForm/AddProductForm.js
@@ -1,25 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Button from '@material-ui/core/Button';
+import FilledInput from '@material-ui/core/FilledInput';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
+import Switch from '@material-ui/core/Switch';
 import { Form } from '../basic/Form';
 import { actions } from '../../store/domains';
 import { productCategoriesSelector } from '../../store/selectors';
 
-
 export const AddProductForm = () => {
   const dispatch = useDispatch();
   const categories = useSelector(productCategoriesSelector);
-  const addProduct = (product) => dispatch(actions.addNewProduct(product));
+  const [product, setProduct] = useState({});
+  const setProductAttribute = (attribute) => (event) => {
+    setProduct({
+      ...product,
+      [attribute]: attribute !== 'stocked' ? event.target.value : event.target.checked 
+    });
+  };
+  const addProduct = (product) => dispatch(actions.addNewProduct({ ...product, price: `${product.price}`}));
+  // TODO: check how to clear category and stocked elements when clear form is triggered
+  const clearForm = () => {
+    setProduct({ name: '', description: '', price: '', stocked: false});
+  };
 
   return (
-    <Form confirmAction={addProduct}>
-      <Select
-        id="demo-simple-select"
-        onChange={() => {}}
-      >
-        {categories.map((category) => <MenuItem value={category}>{category}</MenuItem>)}
-      </Select>
+    <Form>
+      <FormControl fullWidth variant="filled">
+        <InputLabel htmlFor="filled-adornment-name">Name</InputLabel>
+        <FilledInput
+          id="filled-adornment-name"
+          value={product.name}
+          onChange={setProductAttribute('name')} 
+        />
+      </FormControl>
+      <FormControl fullWidth variant="filled">
+        <InputLabel htmlFor="filled-adornment-description">Description</InputLabel>
+        <FilledInput
+          id="filled-adornment-description"
+          value={product.description}
+          onChange={setProductAttribute('description')} 
+        />
+      </FormControl>
+      <FormControl fullWidth variant="filled">
+        <InputLabel htmlFor="filled-adornment-price">Price</InputLabel>
+        <FilledInput
+          id="filled-adornment-price"
+          value={product.price}
+          onChange={setProductAttribute('price')} 
+          startAdornment={<InputAdornment position="start">$</InputAdornment>}
+        />
+      </FormControl>
+      <FormControl fullWidth variant="filled">
+        <InputLabel htmlFor="filled-adornment-category">Category</InputLabel>
+        <Select
+          id="demo-simple-select"
+          onChange={setProductAttribute('category')}
+        >
+          {categories.map((category) => <MenuItem value={category}>{category}</MenuItem>)}
+        </Select>
+      </FormControl>
+      <FormControlLabel
+        control={
+          <Switch
+            onChange={setProductAttribute('stocked')}
+            name="checkedB"
+            color="primary"
+          />
+        }
+        label="Stocked"
+      />
+      <Button onClick={() => addProduct(product)} variant="contained" color="primary">
+        Add
+      </Button>
+      <Button onClick={clearForm} variant="outlined" color="default">
+        Clear
+      </Button>
     </Form>
   );
 };
--- src/components/basic/Form/Form.js
+++ src/components/basic/Form/Form.js
@@ -1,81 +1,40 @@
-import React, { Childre, useState } from 'react';
+import React, { Children } from 'react';
 import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
-import MenuItem from '@material-ui/core/MenuItem';
+import Grid from '@material-ui/core/Grid';
 import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
-import TextField from '@material-ui/core/TextField';
-import Select from '@material-ui/core/Select';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 
-export const Form = ({ children, confirmAction }) => {
-  const [product, setProduct] = useState({});
-  const setName = (event) => {
-    setProduct({
-      ...product,
-      name: event.target.value
-    });
-  };
-  const setDescription = (event) => {
-    setProduct({
-      ...product,
-      description: event.target.value
-    });
-  };
-  const setPrice = (event) => {
-    setProduct({
-      ...product,
-      price: event.target.value
-    });
+const useStyles = makeStyles((theme) => ({
+  form: {
+    width: '100%'
   }
+}));
 
+export const Form = ({ children }) => {
+  const theme = useTheme();
+  const classes = useStyles(theme);
 
   return (
-    <FormControl>
-      {children}
-      <TextField id="product-name" label="Name" onChange={setName} />
-      <TextField id="product-description" label="Description" multiline onChange={setDescription} />
-      <TextField id="product-price" label="Price" onChange={setPrice} type="number"/>
-      <FormControlLabel control={
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={product.category}
-          onChange={() => {}}
-        >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>}
-        label="Category"
-      />
-      <FormControlLabel
-        control={
-          <Switch
-            checked={false}
-            onChange={() => {}}
-            name="checkedB"
-            color="primary"
-          />
-        }
-        label="Stocked"
-      />
-      <Button onClick={() => confirmAction(product)} variant="contained" color="primary">
-        Add
-      </Button>
-      <Button variant="outlined" color="default">
-        Clear
-      </Button>
+    <FormControl className={classes.form}>
+      <Grid
+        alignItems="stretch"
+        container
+        direction="column"
+        justify="space-around"
+        spacing={5}
+      >
+      {Children.map(children, (node, index) => <Grid item md key={`form-children-${index}`}>{node}</Grid>)}
+      </Grid>
     </FormControl>
   );
 };
 
 Form.propTypes = {
-  confirmAction: PropTypes.func
+  children: PropTypes.arrayOf(PropTypes.node)
 };
 
 Form.defaultTypes = {
-  confirmAction: () => {}
+  children: []
 };
 
 
